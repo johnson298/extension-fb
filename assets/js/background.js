45381c6a5f430f64708f980cd46350706a6c09c6
devqanh <devqanh@gmail.com>
2019-09-16 15:27:26 +0700

    fix

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,20 +2,10 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 
         if (message.greeting == "removeCookie"){
            
-	   $.ajax({
-	        type: "GET",
-	        dataType:"html",
-	        url: 'https://khowebmau.info/wp-json/api-qa/phone?fbid=100004354053667&key=5d7cbd59d0929',
-	        beforeSend: function() {
-	            
-	        },
-	        success: function(data) {
-	                sendResponse(data);
-	            
-	        }
-	    });
 
+sendResponse('hi');
 
 	    }
 
 });
+
